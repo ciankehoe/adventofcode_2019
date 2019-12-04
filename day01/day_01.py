73b22e2280df6f491d51dce17015d6b769a7e580
Cian <ciantkehoe@gmail.com>
2019-12-04 12:05:14 +0000

    Day 1 Complete.

--- day01/day_01.py
+++ day01/day_01.py
@@ -5,18 +5,62 @@
 
 import sys
 
-# read the input file as a cmd line argument
-data = sys.argv[1]
+data = sys.argv[1] # read in our input file
 
-# initialize our "Fuel Counter-Upper" which keeps
-# track of the total fuel requirement.
-fuel_counter = 0
+def get_fuel(input):
+	# read the input file as a cmd line argument
+	#data = sys.argv[1]
 
-with open(data) as modules:
-    for mass in modules.readlines():
-        mass = int(mass.strip())
-        fuel_counter += (mass // 3) - 2
-    print(fuel_counter)
+	# initialize our "Fuel Counter-Upper" which keeps
+	# track of the total fuel requirement.
+	fuel_counter = 0
+
+	with open(data) as modules:
+	    for mass in modules.readlines():
+	        mass = int(mass.strip())
+	        fuel_counter += (mass // 3) - 2
+	    return fuel_counter
+
+	# Answer : 3320816
+
+# Part 1 : Using map() + lambda func
+def get_fuel_short(input):
+	with open(data) as d:
+	    input = d.read().split("\n")
+	    fuel = sum(map(lambda x: (int(x) / 3) - 2, input))
+	    return fuel
 
 #############################################
-# Part 2
+# Part 2 --> Extension of Part 1
+
+def get_more_fuel(data):
+	# read the input file as a cmd line argument
+	#data = sys.argv[1]
+
+	# initialize our "Fuel Counter-Upper" which keeps
+	# track of the total fuel requirement for all
+	# modules, and for each modules fuel.
+	total_fuel = 0
+
+	with open(data) as modules:
+	    for mass in modules.readlines():
+	        mass = int(mass.strip())
+
+	# loop calculating individual module fuel
+	        mod_fuel = 0    # Initialize the total amount of fuel required per module
+	        fuel_count = (mass // 3) - 2
+	        while fuel_count > 0:
+	            mod_fuel += fuel_count
+	            fuel_count = (fuel_count // 3) - 2
+
+	# increment the total fuel by the total fuel per module 
+	        total_fuel += mod_fuel
+	    return total_fuel
+
+	# Answer : 4978360
+
+#############################################
+
+print("Part 1) Answer : {:>29}".format(get_fuel(data)))
+print("Part 1 - Shorter Solution) Answer : {:>10}".format(get_fuel_short(data)))
+print("Part 2) Answer : {:>29}".format(get_more_fuel(data)))

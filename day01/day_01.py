937d44565fdd334d088693a2bdeed00d55575a18
Cian <ciantkehoe@gmail.com>
2020-02-20 15:33:02 +0000

    Completed Day02 and condensed Part 1 and 2 into a single file. Cleaned up formatting (PEP8).

--- day01/day_01.py
+++ day01/day_01.py
@@ -1,63 +1,70 @@
-# Day 1: The Tyranny of the Rocket Equation 
+# Day 1: The Tyranny of the Rocket Equation
 
-###########################################
+####################
 # Part 1
+####################
 
 import sys
 
-data = sys.argv[1] # read in our input file
+data = sys.argv[1]  # read in our input file
+
 
 def get_fuel(input):
-	# read the input file as a cmd line argument
-	#data = sys.argv[1]
+    # read the input file as a cmd line argument
+    # data = sys.argv[1]
 
-	# initialize our "Fuel Counter-Upper" which keeps
-	# track of the total fuel requirement.
-	fuel_counter = 0
+    # initialize our "Fuel Counter-Upper" which keeps
+    # track of the total fuel requirement.
+    fuel_counter = 0
 
-	with open(data) as modules:
-	    for mass in modules.readlines():
-	        mass = int(mass.strip())
-	        fuel_counter += (mass // 3) - 2
-	    return fuel_counter
+    with open(data) as modules:
+        for mass in modules.readlines():
+            mass = int(mass.strip())
+            fuel_counter += (mass // 3) - 2
+        return fuel_counter
 
-	# Answer : 3320816
+    # Answer : 3320816
 
+######################################
 # Part 1 : Using map() + lambda func
-def get_fuel_short(input):
-	with open(data) as d:
-	    input = d.read().split("\n")
-	    fuel = sum(map(lambda x: (int(x) / 3) - 2, input))
-	    return fuel
+######################################
 
-#############################################
+def get_fuel_short(input):
+    with open(data) as d:
+        input = d.read().split("\n")
+        fuel = sum(map(lambda x: (int(x) / 3) - 2, input))
+        return fuel
+
+######################################
 # Part 2 --> Extension of Part 1
+######################################
 
 def get_more_fuel(data):
-	# read the input file as a cmd line argument
-	#data = sys.argv[1]
+    # read the input file as a cmd line argument
+    # data = sys.argv[1]
 
-	# initialize our "Fuel Counter-Upper" which keeps
-	# track of the total fuel requirement for all
-	# modules, and for each modules fuel.
-	total_fuel = 0
+    # initialize our "Fuel Counter-Upper" which keeps
+    # track of the total fuel requirement for all
+    # modules, and for each modules fuel.
+    total_fuel = 0
 
-	with open(data) as modules:
-	    for mass in modules.readlines():
-	        mass = int(mass.strip())
+    with open(data) as modules:
+        for mass in modules.readlines():
+            mass = int(mass.strip())
 
-	# loop calculating individual module fuel
-	        mod_fuel = 0    # Initialize the total amount of fuel required per module
-	        fuel_count = (mass // 3) - 2
-	        while fuel_count > 0:
-	            mod_fuel += fuel_count
-	            fuel_count = (fuel_count // 3) - 2
+    # loop calculating individual module fuel
+    # Initialize the total amount of fuel required per module
+            mod_fuel = 0
+            fuel_count = (mass // 3) - 2
+            while fuel_count > 0:
+                mod_fuel += fuel_count
+                fuel_count = (fuel_count // 3) - 2
 
-	# increment the total fuel by the total fuel per module 
-	        total_fuel += mod_fuel
-	    return total_fuel
+    # increment the total fuel by the total fuel per module
+            total_fuel += mod_fuel
+        return total_fuel
 
-	# Answer : 4978360
+    # Answer : 4978360
 
 #############################################
 
--- day02/day_02.py
+++ day02/day_02.py
@@ -1,55 +1,96 @@
-# Day 2: 1202 Program Alarm 
+import copy
 
-###########################################
-# Part 1
+#############################
+# Day 2: 1202 Program Alarm #
+#############################
 
 original_input = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,9,19,23,1,6,23,27,1,10,27,31,1,5,31,35,2,6,35,39,1,5,39,43,1,5,43,47,2,47,6,51,1,51,5,55,1,13,55,59,2,9,59,63,1,5,63,67,2,67,9,71,1,5,71,75,2,10,75,79,1,6,79,83,1,13,83,87,1,10,87,91,1,91,5,95,2,95,10,99,2,9,99,103,1,103,6,107,1,107,10,111,2,111,10,115,1,115,6,119,2,119,9,123,1,123,6,127,2,127,10,131,1,131,6,135,2,6,135,139,1,139,5,143,1,9,143,147,1,13,147,151,1,2,151,155,1,10,155,0,99,2,14,0,0"
 p1_input = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,9,19,23,1,6,23,27,1,10,27,31,1,5,31,35,2,6,35,39,1,5,39,43,1,5,43,47,2,47,6,51,1,51,5,55,1,13,55,59,2,9,59,63,1,5,63,67,2,67,9,71,1,5,71,75,2,10,75,79,1,6,79,83,1,13,83,87,1,10,87,91,1,91,5,95,2,95,10,99,2,9,99,103,1,103,6,107,1,107,10,111,2,111,10,115,1,115,6,119,2,119,9,123,1,123,6,127,2,127,10,131,1,131,6,135,2,6,135,139,1,139,5,143,1,9,143,147,1,13,147,151,1,2,151,155,1,10,155,0,99,2,14,0,0"
 
-###########################################
+#######################
 
-mydata = p1_input.split(",")
+# creating data for part 1 and part 2
+p1_data = p1_input.split(",")
+p2_data = original_input.split(",")
 
-# create / partition the larger list into a list of smaller lists, each of length 4
+# create / partition the larger list into a list of
+# smaller lists, each of length 4.
 # "chunking" the data
-chunks = [mydata[x:x+4] for x in range(0, len(mydata), 4)]
+p1_chunks = [p1_data[x:x+4] for x in range(0, len(p1_data), 4)]
+p2_chunks = [p2_data[x:x+4] for x in range(0, len(p2_data), 4)]
 
 # changes the inner lists of strings to lists of integers // experimenting
-# // by no means the most efficient method!
+# By no means the most efficient method!
 i = 0
-while i < len(chunks):
-    chunks[i] = list(map(int, chunks[i]))
+while i < len(p1_chunks):
+    p1_chunks[i] = list(map(int, p1_chunks[i]))
+    p2_chunks[i] = list(map(int, p2_chunks[i]))
     i += 1
 
-###########################################
+#######################
 
 # variable i --> refers to each individual "instruction".
 # each instruction is a list of 4 elements.
 # e.g [1, 2, 3, 4]
 # 1 is the opcode | 2, 3, 4 are the parameters.
-# the three integers after the opcode indicate where 
+# the three integers after the opcode indicate where
 # the inputs and outputs are, not their values.
 
-def get_answer(prog):
+
+def working_computer(prog):
+    """Solution to Part 1"""
+
     for i in prog:
-        # check opcodes
-        if i[0] > 2: # check for opcode 99 or any unknown opcodes --> exits program
+        # check for opcode 99 or any unknown opcodes --> exits program
+        if i[0] > 2:
             break
+
         # retrieve values of our inputs
-        inp_1 = prog[i[1] / 4][i[1] % 4]
-        inp_2 = prog[i[2] / 4][i[2] % 4]
-        if i[0] == 1: # check for add opcode
+        inp_1 = prog[int(i[1] / 4)][int(i[1] % 4)]
+        inp_2 = prog[int(i[2] / 4)][int(i[2] % 4)]
+
+        if i[0] == 1:  # check for add opcode
             output = inp_1 + inp_2
-        if i[0] == 2: # check for multiply opcode
+        if i[0] == 2:  # check for multiply opcode
             output = inp_1 * inp_2
-        # process / find the position (index) for our output 
-        index = i[3] / 4
-        inner_index = i[3] % 4
+
+        # process / find the position (index) for our output
+        index = int(i[3] / 4)
+        inner_index = int(i[3] % 4)
+
         # move output into correct position
         prog[index][inner_index] = output
 
     return prog[0][0]
 
-###########################################
-print("Answers\n###############")
-print("Part 1) {}".format(get_answer(chunks)))
+
+def find_noun_verb(prog, target):
+    """Solution to Part 2"""
+
+    # create a deepcopy of the original 'program' (list).
+    deep_prog_copy = copy.deepcopy(prog)
+
+    # nested for loops check for every combination of noun and verb
+    for noun in range(0, 100):
+        for verb in range(0, 100):
+
+            deep_prog_copy[0][1] = noun
+            deep_prog_copy[0][2] = verb
+
+            # pass our deep copy to the 'working computer'
+            # calculates value at first position using new noun/verb pair
+            output = working_computer(deep_prog_copy)
+
+            if output == target:
+                return noun, verb, 100 * noun + verb
+            else:
+                # reinitialize our copy of the original program
+                deep_prog_copy = copy.deepcopy(prog)
+    return "There is no matching noun/verb pair."
+
+
+#######################
+
+print("Answers\n---------------")
+print("Part 1) {}".format(working_computer(p1_chunks)))
+print("Part 2) (Noun, Verb, Total) --> {}".format(find_noun_verb(p2_chunks, 19690720)))
